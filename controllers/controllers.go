04c415d23352dbba875336422ca2694c4df61702
Jake Morgan <jmorgan3142001@gmail.com>
2025-12-10 13:56:03 -0500

    USA added to location for consistency and visibility

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -81,7 +81,7 @@ func (c *PortfolioController) Get() {
     c.Data["Title"] = "Jake Morgan | Software Engineer"
     c.Data["Name"] = "Jake Morgan"
     c.Data["Career"] = "Software Engineer"
-    c.Data["Location"] = "Charleston, SC"
+    c.Data["Location"] = "Charleston, SC USA"
     c.Data["Email"] = "jmorgan3142001@gmail.com"
     c.Data["GithubLink"] = "https://github.com/jmorgan3142001"
     c.Data["LinkedinLink"] = "https://www.linkedin.com/in/jake-morgan-/"
